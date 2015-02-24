552dc8e6e04b8afb794d0b7f4fe9f6c6402c68c2
Sami Saarinen <sjsaarinen@gmail.com>
2015-02-24 14:49:12 +0200

    app name changed and updated AndroidManifest

--- app/src/main/java/com/ohtu/wearable/wearabledataservice/MainActivity.java
+++ app/src/main/java/com/ohtu/wearable/wearabledataservice/MainActivity.java
@@ -1,13 +1,23 @@
 package com.ohtu.wearable.wearabledataservice;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
 import java.util.List;
 
 public class MainActivity extends FragmentActivity implements SelectedSensorsInterface {
+
+    private SensorServerService sensorServerService = null;
+    private boolean serviceBound = false;
+
     ViewPager viewpager;
 
     List<Sensor> sensors;
@@ -29,6 +39,67 @@ public class MainActivity extends FragmentActivity implements SelectedSensorsInt
         PagerAdapter padapter = new PagerAdapter(getSupportFragmentManager());
         viewpager.setAdapter(padapter);
 
+        startServerService();
+    }
+
+    //starts server as foreground service
+    private void startServerService() {
+        Intent startIntent = new Intent(this, SensorServerService.class);
+        startIntent.setAction(Constants.ACTION.STARTFOREGROUND_ACTION);
+        startService(startIntent);
+    }
+
+
+
+    //this is called when this activity is started/resumed
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intentBind = new Intent(this, SensorServerService.class);
+        bindService(intentBind, mConnection, 0);
+        if (serviceBound){
+            sensorServerService.startServer(getSensors());
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unbindService(mConnection);
+    }
+
+    //this is called when service is bound to this activit
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            SensorServerService.LocalBinder b = (SensorServerService.LocalBinder) service;
+            sensorServerService = b.getService();
+            sensorServerService.startServer(getSensors());
+            serviceBound = true;
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            sensorServerService = null;
+        }
+    };
+
+
+    /**
+     * Sets sensors available to server
+     * @param List containing all available sensors
+     */
+    public void setServerSensors(List<Sensor> sensors){
+        if (serviceBound){
+            sensorServerService.startServer(sensors);
+        }
+    }
+
+
+    //remove this when passing list from UI is working
+    private List<Sensor> getSensors() {
+        SensorManager mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        return deviceSensors;
     }
 
 }
