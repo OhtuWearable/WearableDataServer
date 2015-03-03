d44970f7bf7eaab3deb60b22cc0dbe94dcd36832
manuligit <riina.vaisanen@gmail.com>
2015-03-03 15:19:42 +0200

    Showing battery from button works

--- app/src/main/java/com/ohtu/wearable/wearabledataservice/fragments/FragmentThree.java
+++ app/src/main/java/com/ohtu/wearable/wearabledataservice/fragments/FragmentThree.java
@@ -1,10 +1,17 @@
 package com.ohtu.wearable.wearabledataservice.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.ohtu.wearable.wearabledataservice.R;
 
@@ -14,7 +21,57 @@ public class FragmentThree extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_three_layout,container,false);
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.button_layout,container,false);
+
+        final LinearLayout layout = (LinearLayout) view.findViewById(R.id.root_layout);
+        final Button bn1 = (Button) view.findViewById(R.id.btnaddnewtext1);
+        final Button bn2 = (Button) view.findViewById(R.id.btnaddnewtext2);
+
+        bn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Code for Activities what will appears when user click button
+
+
+                Toast.makeText(v.getContext(), "Server is on", Toast.LENGTH_SHORT).show();
+                //TextView tv1 = new TextView(v.getContext());
+                //tv1.setText("...");
+                //layout.addView(tv1);
+
+            }
+
+        });
+
+        bn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Code for Activities what will appears when user click button
+                //Bn2 Have to show Battery Level in Tv2(TextArea) or in Notification
+
+                //TextView tv2 = new TextView(v.getContext());
+                Toast.makeText(v.getContext(), "Battery Level: " + batteryLevel(v.getContext()), Toast.LENGTH_LONG).show();
+                //tv2.setText("Battery Level: " + batteryLevel(v.getContext()));
+                //layout.addView(tv2);
+
+            }
+        });
+
+        return view;
     }
+
+    //Method Shows Battery Level
+    private String batteryLevel(Context context) {
+
+        Intent intent;
+        intent = context.registerReceiver(null, new
+                IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        int    level   = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+        int    scale   = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
+        int    percent = (level*100)/scale;
+        return String.valueOf(percent) + "%";
+    }
+
+    //TODO: Start and Stop server if needed
 
 }
