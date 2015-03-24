57292accbb14cfd7c79ef4960135c68d7934c5b1
jannetim <petrutsi@gmail.com>
2015-03-24 19:15:27 +0200

    Test classes for sensors classes

---
+++ app/src/test/java/com/ohtu/wearable/wearabledataservice/sensors/SensorUnitTest.java
@@ -0,0 +1,12 @@
+package com.ohtu.wearable.wearabledataservice.sensors;
+
+import com.ohtu.wearable.wearabledataservice.CustomRobolectricRunner;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by jannetim on 24/03/15.
+ */
+@RunWith(CustomRobolectricRunner.class)
+public class SensorUnitTest {
+}
---
+++ app/src/test/java/com/ohtu/wearable/wearabledataservice/sensors/SensorsHandlerTest.java
@@ -0,0 +1,16 @@
+package com.ohtu.wearable.wearabledataservice.sensors;
+
+import android.hardware.SensorManager;
+
+import com.ohtu.wearable.wearabledataservice.CustomRobolectricRunner;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by jannetim on 24/03/15.
+ */
+@RunWith(CustomRobolectricRunner.class)
+public class SensorsHandlerTest {
+    private SensorManager sensorManager;
+    private SensorsHandler sensorsHandler;
+}
